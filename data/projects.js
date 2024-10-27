ab9c8a4dd22d545954a3b0ce27a4a7e4c171dafb
George Ongoro <gtechong72@gmail.com>
2024-10-27 07:43:29 +0300

    Implemented the 'Project Details Page'.

--- data/projects.js
+++ data/projects.js
@@ -2,24 +2,27 @@ const projects = [
   {
     id: "proj001",
     screenshot: "../../assets/images/project_placeholder.jpg",
-    title: "This is project one",
-    description: "This is a brief description of the first project i created",
-    link: "https",
+    title: "GSCapture - the screen recorder and image editor",
+    description:
+      "A versatile screen recording application to record screen, take screenshots and record audio and export the fully functional video in different video formats.",
+    link: "https://www.github.com/004Ongoro/GSCapture/",
   },
 
   {
     id: "proj002",
     screenshot: "../../assets/images/project_placeholder.jpg",
-    title: "This is project two",
-    description: "This is a brief description of the second project i created",
+    title: "best color picker (bcp)",
+    description:
+      "Inspired by Just Color Picker from annystudio.com, BCP captures color on the screen in different color formats and converts color from one code to another.",
     link: "https://www.github.com/004Ongoro/",
   },
 
   {
     id: "proj003",
     screenshot: "../../assets/images/project_placeholder.jpg",
-    title: "This is project three",
-    description: "This is a brief description of the third project i created",
+    title: "Numsnap",
+    description:
+      "Android Application that scans image files and extract text, it is smart enough to determine which of the text is contact details and saves them to contacts.",
     link: "https://www.github.com/004Ongoro/",
   },
 
@@ -49,3 +52,6 @@ const projects = [
 ];
 
 export default projects;
+
+// Github repo: https://www.github.com/004Ongoro/georgeo.git
+//git remote add origin
--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import projects from "../data/projects.js";
 
-console.log(projects.title);
-
 const renderProjects = () => {
+  //const projectsContainer = document.querySelector(".project_container");
   const projectsContainer = document.querySelector(".project_container");
   const projectCard = document.querySelector(".template");
 
@@ -12,14 +11,40 @@ const renderProjects = () => {
 
     projCard.querySelector("img").src = project.screenshot;
     projCard.querySelector("img").alt = `${project.title} screenshot`;
-    projCard.querySelector("h3").textContent = project.title;
-    projCard.querySelector("p").textContent = project.description;
+    projCard.querySelector("h3").textContent =
+      project.title.length > 20
+        ? project.title.substring(0, 20) + "..."
+        : project.title;
+    projCard.querySelector("p").textContent =
+      project.description.length > 90
+        ? project.description.substring(0, 90) + "..."
+        : project.description;
     projCard.querySelector("a").href = project.link;
 
     projCard.style.display = "flex";
 
     projectsContainer.appendChild(projCard);
+    projCard.addEventListener("click", () => {
+      projectsDetailModal.classList.add("project_details_open");
+      projectsDetailTitle.textContent = project.title;
+      projectsDetailDescr.textContent = project.description;
+    });
   });
 };
 
 document.addEventListener("DOMContentLoaded", renderProjects);
+
+// MANAGE PROJECT MODAL
+
+const projectsDetailModal = document.querySelector(".project_details");
+const projectsDetailTitle = document.querySelector(".det_title");
+const projectsDetailDescr = document.querySelector(".det_description");
+const projectsModalClose = document.getElementById("proj_modal_close");
+let modalOpen = false;
+
+// SHOW/HIDE MODAL
+const closeModal = () => {
+  projectsDetailModal.classList.remove("project_details_open");
+};
+
+projectsModalClose.addEventListener("click", closeModal);
